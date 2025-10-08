85449eea60ed8b428aa9d83576aa812ad9de90f1
Lihindu Perera <lihindu.indudunu.perera@gmail.com>
2025-10-08 13:56:12 +0530

    init Database + screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
+import { initDatabase } from "@/services/database";
 import { Stack } from "expo-router";
 import './globals.css';
 
 export default function RootLayout() {
+  initDatabase();
   
   return (
     <Stack screenOptions={{ headerShown: false }}>
